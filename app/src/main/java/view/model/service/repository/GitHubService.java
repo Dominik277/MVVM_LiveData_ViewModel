eae0488cbf77e9cc1e8c4e76a8878b8c0a282e98
Dominik277 <dominikdoric277@gmail.com>
2020-12-28 22:00:37 +0100

    Implementiran GitHubService interfejs te dodan dependency za Retrofit.

--- app/src/main/java/view/model/service/repository/GitHubService.java
+++ app/src/main/java/view/model/service/repository/GitHubService.java
@@ -1,4 +1,21 @@
 package view.model.service.repository;
 
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import view.model.service.model.Project;
+
 public interface GitHubService {
+
+    String HTTPS_API_GITHUB_URL = "https://api.github.com/";
+
+    @GET("users/{user}/repos")
+    Call<List<Project>> getProjectList(@Path("user") String user);
+
+    @GET("/repos/{user}/{reponame}")
+    Call<Project> getProjectDetails(@Path("user") String user, @Path("reponame") String projectName);
+
+
 }
