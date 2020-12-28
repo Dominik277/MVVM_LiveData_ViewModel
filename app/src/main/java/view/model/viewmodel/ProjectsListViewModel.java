194eacf124143618933fc645a935396f7f43b21a
Dominik277 <dominikdoric277@gmail.com>
2020-12-28 22:11:52 +0100

    Do pola implementirana metoda ProjectsListViewModel.

--- app/src/main/java/view/model/viewmodel/ProjectsListViewModel.java
+++ app/src/main/java/view/model/viewmodel/ProjectsListViewModel.java
@@ -1,4 +1,25 @@
 package view.model.viewmodel;
 
-public class ProjectsListViewModel {
+import android.app.Application;
+import java.util.List;
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import view.model.service.model.Project;
+import view.model.service.repository.ProjectRepository;
+
+public class ProjectsListViewModel extends AndroidViewModel {
+
+    private final LiveData<List<Project>> projectListObservable;
+
+    public ProjectsListViewModel(@NonNull Application application) {
+        super(application);
+
+        projectListObservable = ProjectRepository.getInstance().getProjectList("Google");
+    }
+
+    public LiveData<List<Project>> getProjectListObservable(){
+        return projectListObservable;
+    }
+
 }
