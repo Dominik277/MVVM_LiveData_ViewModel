3cb90982b413ed92c79181d910fbf4587878ef1d
Dominik277 <dominikdoric277@gmail.com>
2020-12-28 22:38:39 +0100

    Dodan interfejs ViewModelSubComponent.

--- app/src/main/java/view/model/di/ViewModelSubComponent.java
+++ app/src/main/java/view/model/di/ViewModelSubComponent.java
@@ -1,4 +1,18 @@
 package view.model.di;
 
+import dagger.Subcomponent;
+import view.model.viewmodel.ProjectViewModel;
+import view.model.viewmodel.ProjectsListViewModel;
+
+@Subcomponent
 public interface ViewModelSubComponent {
+
+    @Subcomponent.Builder
+    interface Builder{
+        ViewModelSubComponent build();
+    }
+
+    ProjectsListViewModel projectListViewModel();
+    ProjectViewModel projectViewModel();
+
 }
