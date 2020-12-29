45701b7897611581fda7b0de66e53e458db032de
Dominik277 <dominikdoric277@gmail.com>
2020-12-29 10:37:04 +0100

    Implementirana FragmentBuildersModule abstraktna klasa.

--- app/src/main/java/view/model/di/FragmentBuildersModule.java
+++ app/src/main/java/view/model/di/FragmentBuildersModule.java
@@ -1,4 +1,16 @@
 package view.model.di;
 
-public class FragmentBuildersModule {
+import dagger.Module;
+import view.model.view.ui.ProjectFragment;
+import view.model.view.ui.ProjectListFragment;
+
+@Module
+public abstract class FragmentBuildersModule {
+
+    @ContributesAndroidInjector
+    abstract ProjectFragment contributeProjectFragment();
+
+    @ContributesAndroidInjector
+    abstract ProjectListFragment contributeProjectListFragment();
+
 }
