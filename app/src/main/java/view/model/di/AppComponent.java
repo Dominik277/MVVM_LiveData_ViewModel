905d47f03e42798e5cc95ec5a247fafe5f9b5ad7
Dominik277 <dominikdoric277@gmail.com>
2020-12-29 10:41:57 +0100

    Implementirana AppComponent klasa.

--- app/src/main/java/view/model/di/AppComponent.java
+++ app/src/main/java/view/model/di/AppComponent.java
@@ -1,4 +1,27 @@
 package view.model.di;
 
+
+import android.app.Application;
+import javax.inject.Singleton;
+import dagger.BindsInstance;
+import dagger.Component;
+import view.model.MVVMApplication;
+
+@Singleton
+@Component(modules = {
+        AndroidInjectionModule.class,
+        AppModule.class,
+        MainActivityModule.class
+})
+
 public interface AppComponent {
+
+    @Component.Builder
+    interface Builder{
+        @BindsInstance Builder application(Application application);
+        AppComponent build();
+    }
+
+    void inject(MVVMApplication mvvmApplication);
+
 }
