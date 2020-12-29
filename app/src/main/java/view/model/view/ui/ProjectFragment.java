ba812345e24928ac607c2451dd757c78419bb9e4
Dominik277 <dominikdoric277@gmail.com>
2020-12-29 11:06:32 +0100

    Implementirana ProjectFragment klasa.

--- app/src/main/java/view/model/view/ui/ProjectFragment.java
+++ app/src/main/java/view/model/view/ui/ProjectFragment.java
@@ -1,4 +1,24 @@
 package view.model.view.ui;
 
-public class ProjectFragment {
+import android.os.Bundle;
+
+import javax.inject.Inject;
+
+import androidx.annotation.Nullable;
+import androidx.lifecycle.ViewModelProvider;
+import view.model.di.Injectable;
+import view.model.viewmodel.ProjectViewModel;
+
+public class ProjectFragment extends LifecycleFragment implements Injectable {
+
+    @Inject
+    ViewModelProvider.Factory viewModelFactory;
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        final ProjectViewModel viewModel = ViewModelProviders.of(this, viewModelFactory)
+                .get(ProjectViewModel.class);
+
 }
