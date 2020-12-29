0be3d6ca8754f006be7cd85d4530ca00aaee92b1
Dominik277 <dominikdoric277@gmail.com>
2020-12-29 10:29:20 +0100

    Implementirana void init metoda unutr AppInjector klase.

--- app/src/main/java/view/model/di/AppInjector.java
+++ app/src/main/java/view/model/di/AppInjector.java
@@ -1,9 +1,57 @@
 package view.model.di;
 
+import android.app.Activity;
+import android.app.Application;
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import view.model.MVVMApplication;
+
 public class AppInjector {
 
     private AppInjector(){}
 
+    public static void init(MVVMApplication mvvmApplication){
+        DaggerAppComponent.builder().application(mvvmApplication)
+                .build().inject(mvvmApplication);
 
+        mvvmApplication
+                .registerActivityLifecycleCallbacks(new Application.ActivityLifecycleCallbacks() {
+                    @Override
+                    public void onActivityCreated(@NonNull Activity activity, @Nullable Bundle savedInstanceState) {
+                        handleActivity(activity);
+                    }
+
+                    @Override
+                    public void onActivityStarted(@NonNull Activity activity) {
+
+                    }
+
+                    @Override
+                    public void onActivityResumed(@NonNull Activity activity) {
+
+                    }
+
+                    @Override
+                    public void onActivityPaused(@NonNull Activity activity) {
+
+                    }
+
+                    @Override
+                    public void onActivityStopped(@NonNull Activity activity) {
+
+                    }
+
+                    @Override
+                    public void onActivitySaveInstanceState(@NonNull Activity activity, @NonNull Bundle outState) {
+
+                    }
+
+                    @Override
+                    public void onActivityDestroyed(@NonNull Activity activity) {
+
+                    }
+                });
+    }
 
 }
