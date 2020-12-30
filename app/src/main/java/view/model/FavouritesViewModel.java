9f8c2c97f9ab4ab17dcc4becf02b842b2cce1cee
Dominik277 <dominikdoric277@gmail.com>
2020-12-30 12:25:30 +0100

    Zavrsena metoda loadFavs().

--- app/src/main/java/view/model/FavouritesViewModel.java
+++ app/src/main/java/view/model/FavouritesViewModel.java
@@ -1,12 +1,15 @@
 package view.model;
 
 import android.app.Application;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
+import view.model.db.DbSettings;
 import view.model.db.FavouritesDbHelper;
 
 public class FavouritesViewModel extends AndroidViewModel {
@@ -44,6 +47,25 @@ public class FavouritesViewModel extends AndroidViewModel {
     }
 
     private void loadFavs(){
+
+        mFavs.clear();
+
+        SQLiteDatabase db = mFavHelper.getReadableDatabase();
+        Cursor cursor = db.query(DbSettings.DBEntry.TABLE,
+                new String[]{
+                        DbSettings.DBEntry._ID,
+                        DbSettings.DBEntry.COL_FAV_URL,
+                        DbSettings.DBEntry.COL_FAV_DATE
+                },null,null,null,null,null);
+        while (cursor.moveToNext()){
+            int idxId = cursor.getColumnIndex(DbSettings.DBEntry._ID);
+            int idxUrl = cursor.getColumnIndex(DbSettings.DBEntry.COL_FAV_URL);
+            int idxDate = cursor.getColumnIndex(DbSettings.DBEntry.COL_FAV_DATE);
+            mFavs.add(new Favourites(cursor.getLong(idxId),cursor.getString(idxUrl)
+                                    ,cursor.getLong(idxDate)));
+        }
+        cursor.close();
+        db.close();
 
     }
 
