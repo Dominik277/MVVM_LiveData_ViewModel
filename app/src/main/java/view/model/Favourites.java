2a9c3e886130df3a1bf2038bab29a1c8ca071904
Dominik277 <dominikdoric277@gmail.com>
2020-12-30 12:01:29 +0100

    Implementirana Favourites klasa.

--- app/src/main/java/view/model/Favourites.java
+++ app/src/main/java/view/model/Favourites.java
@@ -1,4 +1,21 @@
 package view.model;
 
 public class Favourites {
+
+    public long mId;
+    public String mUrl;
+    public long mDate;
+
+    public Favourites(long id, String name, long date) {
+        this.mId = id;
+        this.mUrl = name;
+        this.mDate = date;
+    }
+
+    public Favourites(Favourites favourites){
+        mId = favourites.mId;
+        mUrl = favourites.mUrl;
+        mDate = favourites.mDate;
+    }
+
 }
