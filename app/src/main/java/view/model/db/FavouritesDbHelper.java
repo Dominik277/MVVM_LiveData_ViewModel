3cbffd0b16ab008c306285ce789c76ea40ebbc01
Dominik277 <dominikdoric277@gmail.com>
2020-12-30 11:54:19 +0100

    Dodana klasa FavouritesDbHelper koja nasljeđuje SQLiteOpenHelper klasu te zbog toga moramo implementirati dvije metode onCreate i onUpdate.

--- app/src/main/java/view/model/db/FavouritesDbHelper.java
+++ app/src/main/java/view/model/db/FavouritesDbHelper.java
@@ -1,4 +1,24 @@
 package view.model.db;
 
-public class FavouritesDbHelper {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class FavouritesDbHelper extends SQLiteOpenHelper {
+
+    public FavouritesDbHelper(@Nullable Context context) {
+        super(context,DbSettings.DB_NAME,null,DbSettings.DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
 }
