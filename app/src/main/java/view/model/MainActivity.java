b2e81444240ded757539e2c1952f1e766e9b7d8e
Dominik277 <dominikdoric277@gmail.com>
2020-12-30 12:58:30 +0100

    Dodana jos jedna klasa unutar MainActivity klase a to je FavouritesAdapter klasa.

--- app/src/main/java/view/model/MainActivity.java
+++ app/src/main/java/view/model/MainActivity.java
@@ -1,4 +1,38 @@
 package view.model;
 
-public class MainActivity {
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import java.util.List;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FavouritesAdapter mFavAdapter;
+    private FavouritesViewModel mFavViewModel;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void deteFav(View view){
+        View parent = (View) view.getParent();
+        int position = (int)parent.getTag(R.id.POS);
+        Favourites favourites = mFavAdapter.getItem(position);
+        mFavViewModel.removeFav(favourites.mId);
+        mFavAdapter.remove(favourites);
+    }
+
+    public class FavouritesAdapter extends ArrayAdapter<Favourites>{
+
+        public FavouritesAdapter(@NonNull Context context, int resource, List<Favourites> todos) {
+            super(context, resource,todos);
+        }
+    }
+
 }
